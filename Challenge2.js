5fd07e537e4c6e0f8b287c3067342c79eca9908f
Qusai Issa <qusai.2010.123@hotmail.com>
2023-03-19 21:28:55 +0200

    Finish challenge 2

---
+++ Challenge2.js
@@ -0,0 +1,24 @@
+
+function  findMissing(arr) {
+// for this challenge first i find the sum of the array
+// then i compare it to the sum that should be givin if it was full
+// so i can know the missing element without sorting
+let N = arr.length;
+let sum = 0;
+let total = (N) * (N + 1) / 2;
+
+for (let i =0; i < N ; i++){
+    sum += arr[i];
+}
+
+return total-sum;
+}
+
+
+let arr = [ 1, 2, 3, 0 ];
+
+// Function call
+let miss = findMissing(arr);
+console.log(miss)
+document.write(miss);
+
